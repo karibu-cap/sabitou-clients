df8a054c14fa304b3fb6960a0c98ef9b2e26637c
warrenrhodes <webanalyse237@gmail.com>
2024-09-30 23:47:30 +0100

    Adds logger and state maager package `Get`

---
+++ sabitou_clients/lib/services/internationalization/app_translations.dart
@@ -0,0 +1,20 @@
+// Implement GetX Translations
+import 'package:get/get.dart';
+
+import 'internationalization.dart';
+
+/// The [AppTranslations] class is a subclass of the [Translations] class
+/// that is used to provide translations for the app.
+class AppTranslations extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys {
+    final result = <String, Map<String, String>>{};
+    for (final entry in AppInternationalization.translations.entries) {
+      for (final langEntry in entry.value.entries) {
+        result.putIfAbsent(langEntry.key, () => {})[entry.key] =
+            langEntry.value;
+      }
+    }
+    return result;
+  }
+}
---
+++ sabitou_clients/lib/utils/logger.dart
@@ -0,0 +1,52 @@
+import 'package:logger/logger.dart';
+
+/// The logger configuration.
+final class LoggerApp {
+  /// The class name.
+  final String? className;
+
+  /// Log a message at level [Level.warning].
+  void warning(String message, {Object? error, StackTrace? stackTrace}) =>
+      logger.w(
+        className == null ? message : '$className: $message',
+        error: error,
+        stackTrace: stackTrace,
+      );
+
+  /// Log a message at level [Level.info].
+  void info(String message) => Logger(
+        printer: SimplePrinter(
+          printTime: true,
+        ),
+      ).i(
+        className == null ? message : '$className: $message',
+      );
+
+  /// Log a message at level [Level.log].
+  void log(String message) => Logger(
+        printer: SimplePrinter(
+          printTime: true,
+        ),
+      ).d(
+        className == null ? message : '$className: $message',
+      );
+
+  /// Log a message at level [Level.e].
+  void severe(String message, {Object? error, StackTrace? stackTrace}) =>
+      logger.e(
+        className == null ? message : '$className: $message',
+        error: error,
+        stackTrace: stackTrace,
+      );
+
+  /// Constructor of new [LoggerApp].
+  LoggerApp([this.className]);
+
+  /// The logger configuration.
+  final Logger logger = Logger(
+    printer: PrettyPrinter(
+      dateTimeFormat: DateTimeFormat.onlyTimeAndSinceStart,
+    ),
+    level: Level.trace,
+  );
+}
