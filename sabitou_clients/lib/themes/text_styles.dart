c423f5a2831e1d2c93dc238b2f1ea02dc58e1257
warren rhodes <webanalyse237@gmail.com>
2024-10-02 03:44:13 +0100

    Adds the app theme (#20)

---
+++ sabitou_clients/lib/themes/text_styles.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: public_member_api_docs
+
+import 'dart:ui';
+
+/// The font size constants for the application.
+final class AppFontSize {
+  static const double sm = 12.0;
+  static const double md = 16.0;
+  static const double lg = 20.0;
+  static const double xl = 24.0;
+  static const double xl2 = 32.0;
+}
+
+final class AppFontWeight {
+  static const regular = FontWeight.w400;
+  static const medium = FontWeight.w500;
+  static const bold = FontWeight.w700;
+  static const semiBold = FontWeight.w600;
+}
