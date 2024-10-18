b4d0b84cde62a8d06472b8231376e379e1153ac0
kurocifer <tiza@karibu-cap.com>
2024-10-18 19:02:48 +0100

    Add golden test for profile page.

---
+++ sabitou_clients/test/screens/profile/profile_view_golden_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:sabitou_clients/screens/profile/profile_view.dart';
+
+import '../../goldens.dart';
+
+
+class CreateProfileViewWidgetds extends StatelessWidget {
+  const CreateProfileViewWidgetds({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfileView();
+  }
+}
+
+void main() {
+  group('Goldens', () {
+    testGoldens('Profile page', (tester) async {
+      await multiScreenMultiLocaleGolden(
+        tester,
+        const CreateProfileViewWidgetds(),
+        'profile_page',
+      );
+    });
+  });
+}
